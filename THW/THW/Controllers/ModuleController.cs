c9a001e04745a23a49520fd46c123a788bec3677
vntrungtin1909 <91836645+vntrungtin1909@users.noreply.github.com>
2023-11-25 19:24:55 +0700

    menu_userinterface
    menu va giao dien nguoi dung

---
+++ THW/THW/Controllers/ModuleController.cs
@@ -0,0 +1,21 @@
+﻿using MyClass.DAO;
+using MyClass.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace THW.Controllers
+{
+    public class ModuleController : Controller
+    {
+        MenusDAO menusDAO = new MenusDAO();
+        // GET: Module
+        public ActionResult MainMenu()
+        {
+            List<Menus> list = menusDAO.getListByParentId(0);
+           return View("MainMenu", list);
+        }
+    }
+}
